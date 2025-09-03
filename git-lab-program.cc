1427ca3c4a509f977eb6aa3fb19ac60fee199adb
Esther Ojo <eo787725@ohio.edu>
2025-09-03 14:16:11 -0400

    Initial

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Esther Ojo>
+ *        Date: <9/3/2025>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
