2db4b36c0db93508ae1ad2bfc15bff4cc99df535
Ore-zie <eo787725@ohio.edu>
2026-09-01 18:27:45 -0400

    Done

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Esther Ojo
+ *        Date: 9/1/2026
+ * Description: print outs the message hello git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+
+    /*add code*/
+    return 0;
+}// main
